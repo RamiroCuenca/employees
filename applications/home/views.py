d7b2564b343a27865ae843a473712e8dc7d9330a
RamiroCuenca <rami_cuenca@hotmail.com>
2020-11-07 14:39:03 -0300

    Added 'templates' folder and changed 'base' settings so that it recognize it as default

--- applications/home/views.py
+++ applications/home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 # Generic views from Django
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
-# We work with classes as views
+'''
+Django works with class based views
+There are different generic views, some of them are:
+> TemplateView --> Have a "template_name" var wich associate the 
+view with a html file located always in "templates" folder  
+> ListView --> Used to list objects or records
+> CreateView
+> UpdateView
+'''
 class TestView(TemplateView):
-    template_name = 'test.html' # Remember to create the template
+    template_name = 'home/test.html' # Remember to create the template
