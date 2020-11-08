8e0bf19500292423830c9a4f0cfb49006f4203f4
RamiroCuenca <rami_cuenca@hotmail.com>
2020-11-08 09:29:41 -0300

    Created 'Staff' model and altered 'Department' fields

--- applications/departments/models.py
+++ applications/departments/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Department(models.Model):
-    name = models.CharField('Name', max_length=50)
-    tag = models.CharField('Tag', max_length=15)
+    name = models.CharField('Name', max_length=50, editable = False)
+    tag = models.CharField('Tag', max_length=15, blank = True, unique = True)
     is_active = models.BooleanField('Is Active', default = True)
 
     def __str__(self):
-        return self.id + ' - ' + self.name + ' - ' + self.tag + ' - ' + str(self.is_active) 
+        return str(self.id) + ' - ' + self.name + ' - ' + self.tag + ' - ' + str(self.is_active)
--- applications/staff/models.py
+++ applications/staff/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from applications.departments.models import Department
+
+class Staff(models.Model):
+    ''' Model for employees table '''
+
+    JOB_CHOICES = (
+        ('0', 'Marketer'),
+        ('1', 'Administrator'),
+        ('2', 'HR Specialist'),
+        ('3', 'Accountant'),
+        ('4', 'Worker'),
+        ('5', 'Salesman'),
+        ('6', 'Other'),
+    )
+
+    first_name = models.CharField('Name', max_length = 50)
+    last_name = models.CharField('Last Name', max_length = 50)
+    job = models.CharField('Job', max_length = 1, choices = JOB_CHOICES)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    #image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
